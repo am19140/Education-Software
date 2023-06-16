0e2a6eabf8a7bc647c6a18eaa6284228a71e7826
Mariana Petropoulou <marianapetropoulou@gmail.com>
2023-06-16 15:21:15 +0300

    created registration view page only

---
+++ Education-Software/Models/RegistrationModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Education_Software.Models
+{
+    public class RegistrationModel
+    {
+        [Required]
+        public string? username { get; set; }
+
+        [Required]
+        public string? password { get; set; }
+        [Required]
+        public string firstname { get; set; }
+
+        [Required]
+        public string lastname { get; set; }
+
+        public string? profileImage { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$", ErrorMessage = "Insert a valid email address")]
+
+        public string email { get; set; }
+
+        [Required]
+        public string gender { get; set; }
+
+
+        [Required]
+        [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Invalid Phone Number.")]
+        public string phone_number { get; set; }
+    }
+}
